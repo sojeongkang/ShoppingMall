443c58bee910c57b8c8622f0278fcd5dbc64ecd2
sojeongkang <sojeong5879@gmail.com>
2022-01-06 18:48:47 +0900

    연관 관계 매핑

---
+++ src/main/java/com/shop/entity/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.shop.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+@EntityListeners(value = {AuditingEntityListener.class})
+@MappedSuperclass
+@Getter
+//등록일, 수정일, 등록자, 수정자를 모두 갖는 엔티티가 상속받을 클래스
+public abstract class BaseEntity extends BaseTimeEntity {
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    private String modifiedBy;
+}
---
+++ src/main/java/com/shop/entity/BaseTimeEntity.java
@@ -0,0 +1,28 @@
+package com.shop.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@EntityListeners(value = {AuditingEntityListener.class})
+@MappedSuperclass
+@Getter
+@Setter
+//등록자, 수정자를 갖지 않는 엔티티가 상속받을 클래스
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime regTime;
+
+    @LastModifiedDate
+    private LocalDateTime updateTime;
+
+}
